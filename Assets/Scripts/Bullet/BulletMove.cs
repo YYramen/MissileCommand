7c206e5e3005d60c37bcc50048f50fda91815427
YUSUKE <yoshidayusuke1124@gmail.com>
2022-11-04 23:42:03 +0900

    色々クラス作成

--- Assets/Scripts/Bullet/BulletMove.cs
+++ Assets/Scripts/Bullet/BulletMove.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.EventSystems;
 
 /// <summary>
 /// ’e‚Ì“®‚«
 /// </summary>
-public class BulletMove : MonoBehaviour
+public class BulletMove : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] float _bulletSpeed = 3f;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+    }
 }
