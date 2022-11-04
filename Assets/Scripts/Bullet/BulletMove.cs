bb5f93adeca0c1558d78db11c0bcf039498cddfc
YUSUKE <yoshidayusuke1124@gmail.com>
2022-11-04 16:59:23 +0900

    DoTween導入

---
+++ Assets/Scripts/Bullet/BulletMove.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ’e‚Ì“®‚«
+/// </summary>
+public class BulletMove : MonoBehaviour
+{
+    [SerializeField] float _bulletSpeed = 3f;
+}
