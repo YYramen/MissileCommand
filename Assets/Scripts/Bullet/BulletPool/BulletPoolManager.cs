355d4c05849263ccd7436ca1925ced23b30453bb
YusukeYoshida <87760101+YYramen@users.noreply.github.com>
2022-11-08 18:56:04 +0900

    Androidビルドに対応

--- Assets/Scripts/Bullet/BulletPool/BulletPoolManager.cs
+++ Assets/Scripts/Bullet/BulletPool/BulletPoolManager.cs
@@ -1,50 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Pool;
+//using UniRx.Toolkit;
+//using UnityEngine;
 
-/// <summary>
-/// バレットのObjectPool管理用クラス
-/// </summary>
-public class BulletPoolManager : MonoBehaviour
-{
-    ObjectPool<GameObject> _pool;
+//namespace Samples
+//{
+//    /// <summary>
+//    /// ExplosionEffectのPool
+//    /// </summary>
+//    public class EffectPool : ObjectPool<ExplosionEffect>
+//    {
+//        private readonly ExplosionEffect _prefab;
+//        private readonly Transform _parenTransform;
 
-    [SerializeField] GameObject _prefab;
+//        //コンストラクタ
+//        public EffectPool(Transform parenTransform, ExplosionEffect prefab)
+//        {
+//            _parenTransform = parenTransform;
+//            _prefab = prefab;
+//        }
 
-    public GameObject Prefab { get => _prefab; set => _prefab = value; }
+//        /// <summary>
+//        /// オブジェクトの追加生成時に実行される
+//        /// </summary>
+//        protected override ExplosionEffect CreateInstance()
+//        {
+//            //新しく生成
+//            var e = GameObject.Instantiate(_prefab);
 
-    void Awake()
-    {
-        _pool = new ObjectPool<GameObject>(OnCreatePooledObject, OnGetFromPool, OnReleaseToPool, OnDestroyPooledObject);
-    }
-    GameObject OnCreatePooledObject()
-    {
-        return Instantiate(Prefab);
-    }
-    void OnGetFromPool(GameObject obj)
-    {
-        obj.SetActive(true);
-    }
-    void OnReleaseToPool(GameObject obj)
-    {
-        obj.SetActive(false);
-    }
-    void OnDestroyPooledObject(GameObject obj)
-    {
-        Destroy(obj);
-    }
-    public GameObject GetGameObject(GameObject prefab, Vector3 position, Quaternion rotation)
-    {
-        Prefab = prefab;
-        GameObject obj = _pool.Get();
-        Transform tf = obj.transform;
-        tf.position = position;
-        tf.rotation = rotation;
-        return obj;
-    }
-    public void ReleaseGameObject(GameObject obj)
-    {
-        _pool.Release(obj);
-    }
-}
+//            //ヒエラルキーが散らからないように一箇所にまとめる
+//            e.transform.SetParent(_parenTransform);
+
+//            return e;
+//        }
+//    }
+//}
