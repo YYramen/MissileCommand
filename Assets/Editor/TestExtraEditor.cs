7fc4782d016b339078ac26c76fe901d7729fad5f
YusukeYoshida <87760101+YYramen@users.noreply.github.com>
2022-12-09 14:23:46 +0900

    GameObjectを表示したい

--- Assets/Editor/TestExtraEditor.cs
+++ Assets/Editor/TestExtraEditor.cs
@@ -2,48 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.IMGUI;
 
 public class TestExtraEditor : EditorWindow
 {
+    List<GameObject> _sampleObjects = new List<GameObject>();
+    GameObject _selectedObjects;
+
+    private Vector2 _dataScrollPosition;
+    private Vector2 _parameterScrollPosition;
 
     [MenuItem("Editor/Sample")]
     private static void Create()
     {
-        GetWindow<EditorWindowSample>("サンプル");
+        GetWindow<TestExtraEditor>("サンプル");
     }
 
-    public class EditorWindowSample : EditorWindow
-    {
-        private Vector2 _dataScrollPosition;
-        private Vector2 _parameterScrollPosition;
-
-
     private void OnGUI()
+    {
+        using (new GUILayout.HorizontalScope())
         {
-            using (new GUILayout.HorizontalScope())
-            {
-                UpdateLayoutData();
-                UpdateLayoutParameter();
-            }
+            UpdateLayoutGameObjects();
+            UpdateLayoutInspector();
         }
+    }
 
-        private void UpdateLayoutData()
+    private void UpdateLayoutGameObjects()
+    {
+        using (GUILayout.ScrollViewScope scroll = new GUILayout.ScrollViewScope(_dataScrollPosition, EditorStyles.helpBox, GUILayout.Width(150)))
         {
-            using (GUILayout.ScrollViewScope scroll = new GUILayout.ScrollViewScope(_dataScrollPosition, EditorStyles.helpBox, GUILayout.Width(150)))
-            {
-                _dataScrollPosition = scroll.scrollPosition;
+            _dataScrollPosition = scroll.scrollPosition;
 
-                GUILayout.Label("data");
+            GenericMenu menu = new GenericMenu();
+            if (Event.current.type == EventType.ContextClick && Event.current.button == 1)
+            {
+                menu.AddItem(new GUIContent("AddSample"), false, () =>
+                {
+                    _sampleObjects.Add(Instantiate<GameObject>(_selectedObjects));
+                    _selectedObjects = _sampleObjects[_sampleObjects.Count - 1];
+                });
             }
+
+            // メニュー表示
+            if (menu.GetItemCount() > 0)
+            {
+                menu.ShowAsContext();
+                Event.current.Use();
+            }
+
+            GUILayout.Label("GameObjects");
         }
-        private void UpdateLayoutParameter()
-        {
-            using (GUILayout.ScrollViewScope scroll = new GUILayout.ScrollViewScope(_parameterScrollPosition, EditorStyles.helpBox))
-            {
-                _parameterScrollPosition = scroll.scrollPosition;
+    }
 
-                GUILayout.Label("parameter");
-            }
+    private void UpdateLayoutInspector()
+    {
+        using (GUILayout.ScrollViewScope scroll = new GUILayout.ScrollViewScope(_parameterScrollPosition, EditorStyles.helpBox))
+        {
+            _parameterScrollPosition = scroll.scrollPosition;
+
+            GUILayout.Label("Inspector");
         }
     }
 }
+
